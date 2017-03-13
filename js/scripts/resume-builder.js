adf74775437340c32cacb8062f7b8476ac131202
Robert Weber <rwebaz@me.com>
2017-03-13 16:06:06 -0700

    Add parse-a-url (.htm)
    Accompanying (.js); Improve resume-builder; advance shell template;
    improve footer-vs

--- js/scripts/resume-builder.js
+++ js/scripts/resume-builder.js
@@ -4,24 +4,32 @@
 jQuery(function($) {
     window.alert(`¡El DOM está listo!`);
 });
-//Declare and initialize the method No Conflict to the global variable jKey
+//Declare a No Conflict method
 var jKey = jQuery.noConflict();
-//Append string to the parent tag of the id #1
+//New jKey function
 jKey(function($) {
-    //Append String to the parent tag of the id
-    jKey('#test').append("The Author");
+    //Declare and initialize the var target id
+    var test = jKey('#test');
+    //Append String to the parent tag of the target id
+    test.append("The Author");
 });
-//Append string to the parent tag of the id #2
+//New jKey function
 jKey(function($) {
-    //Declare and initialize the String to the global variable author
+    //Declare and initialize the String to author
     var author = "Robert Hempaz, PhD Trichometry";
     //Append author to the parent tag of the id
     jKey('#rht').append(`${author}`);
 });
+//New jKey Function
+
 //Build a URL programatically
 jKey(function($) {
+    //Declare and initialize the var target id
+    var mminail = jKey('#mminail');
+    var url = 'http://cannabuds.us';
+    var a = jKey('<a>', { href: url });
     //Target the id, create an 'a' tag, add a 'class', initialize the 'text', initialize the 'attribute(s)', append the result to the given 'span' tag
-    jKey('#mminail').createElement('a').addClass('neon-text').text('MMINAIL').attr('href', 'http://cannabuds.us').appendTo('span');
+    mminail.a.addClass('neon-text').text('MMINAIL').attr('title', 'Click to Jump To ...').appendTo('span');
 });
 //Check to see if all assets have been loaded
 jKey('window').load(function($) {
