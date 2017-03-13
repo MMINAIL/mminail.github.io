4ca7888e8c5f602fd2a10131db8d53c19f555ce0
Robert Weber <rwebaz@me.com>
2017-03-12 18:58:11 -0700

    Add jQuery folder to nav
    Improve footer-vs; add jQuery folder icons to glyphicons; improve
    resume-builder (.js); improve ins code aside; adjust css to accommodate

--- js/scripts/resume-builder.js
+++ js/scripts/resume-builder.js
@@ -1,3 +1,29 @@
 //Resume Builder Program
 'use strict';
-$("#test").append("The Author");
+//Test for DOM readiness
+jQuery(function($) {
+    window.alert(`¡El DOM está listo!`);
+});
+//Declare and initialize the method No Conflict to the global variable jKey
+var jKey = jQuery.noConflict();
+//Append string to the parent tag of the id #1
+jKey(function($) {
+    //Append String to the parent tag of the id
+    jKey('#test').append("The Author");
+});
+//Append string to the parent tag of the id #2
+jKey(function($) {
+    //Declare and initialize the String to the global variable author
+    var author = "Robert Hempaz, PhD Trichometry";
+    //Append author to the parent tag of the id
+    jKey('#rht').append(`${author}`);
+});
+//Build a URL programatically
+jKey(function($) {
+    //Target the id, create an 'a' tag, add a 'class', initialize the 'text', initialize the 'attribute(s)', append the result to the given 'span' tag
+    jKey('#mminail').createElement('a').addClass('neon-text').text('MMINAIL').attr('href', 'http://cannabuds.us').appendTo('span');
+});
+//Check to see if all assets have been loaded
+jKey('window').load(function($) {
+    alert(`All of the assets are loaded.`);
+});
