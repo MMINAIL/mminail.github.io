63412b98402d43287002064f1a740a6de4e3974f
Robert Weber <rwebaz@me.com>
2017-03-16 16:22:41 -0700

    Add starter template to js directory
    Improve class-donor-constructor; Classes (.htm); Adjust nav; Improve
    resume-builder (.js) and corresponding footer-vs (.htm)

--- js/scripts/resume-builder.js
+++ js/scripts/resume-builder.js
@@ -1,36 +1,49 @@
 //Resume Builder Program
 'use strict';
-//Test for DOM readiness
+//Test for DOM readiness; Show current date
 jQuery(function($) {
-    window.alert(`¡El DOM está listo!`);
+    var date = new Date();
+    window.alert(`¡El DOM está listo! The current session between client and server has begun on: ${date}`);
 });
 //Declare a No Conflict method
 var jKey = jQuery.noConflict();
+//New jKey function
+
 //Hold the dollar sign
 /* var jkey2 = $.noConflict(); */
+
 //New jKey function
 jKey(function($) {
-    //Declare and initialize the var target id
+    //Declare and initialize the var target id wrapper set
     var test = jKey('#test');
+    /* //Add a class to the wrapper set; 
+    .addClass('neon-text') */
     //Append String to the parent tag of the target id
     test.append("The Author");
 });
 //New jKey function
 jKey(function($) {
+    //Declare and initialize the var target id wrapper set
+    var rht = jKey('#rht');
     //Declare and initialize the String to author
     var author = "Robert Hempaz, PhD Trichometry";
+    /* //Add a class to the wrapper set; 
+    .addClass('neon-text') */
     //Append author to the parent tag of the id
-    jKey('#rht').append(`${author}`);
+    rht.append(`${author}`);
 });
-//New jKey Function
-
-jKey(function($){
+//Build a URL programatically #1
+jKey(function($) {
+    //Declare and initialize the var target id wrapper set
     var mminail = jKey('#mminail');
+    //Add an element; Add a class
     mminail.add('').addClass('neon-text')
+    //Add the text-label of the URL; Add the attributes of the URL
     .text('MMINAIL').attr('title', 'Click to Jump To ...')
+    //Append the URL to the 'span' tag of the id
     .appendTo('');
 });
-//Build a URL programatically
+//Build a URL programatically #2
 /* jKey(function($) {
     //Declare and initialize the var target id
     var mminail = jKey('#mminail');
@@ -39,7 +52,10 @@ jKey(function($){
     //Target the id, create an 'a' tag, add a 'class', initialize the 'text', initialize the 'attribute(s)', append the result to the given 'span' tag
     mminail.add('a').addClass('neon-text').text('MMINAIL').attr('title', 'Click to Jump To ...').appendTo('span');
 }); */
-//Check to see if all assets have been loaded
-jKey('window').load(function($) {
-    alert(`All of the assets are loaded.`);
+
+//Check to see if all of the page assets have been loaded
+jKey(function($) {
+    //Declare and initialize the var target id wrapper set
+    var document = jKey('#document');
+    document.load.alert(`All of the assets are now loaded.`);
 });
