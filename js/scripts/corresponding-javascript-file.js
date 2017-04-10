7e3c2d3f2b178f8ada5b29dcbbda65f9c8881a3c
Robert Weber <rwebaz@me.com>
2017-04-10 14:48:36 -0700

    Adj nav to accommodate new liquid wrappers (.htm)
    Improve css font faces; Add font awesome

---
+++ js/scripts/corresponding-javascript-file.js
@@ -0,0 +1,13 @@
+//Name-O-Program
+'use strict';
+//Test for DOM readiness; Show current date
+/* jQuery(function($) {
+    var date = new Date();
+    window.alert(`¡El DOM está listo! The current session between client and server has begun on ${date}`);
+}); */
+//Declare a No Conflict method
+var jKey = jQuery.noConflict();
+//New jKey function
+/* jKey(function($) {
+    //Build Ur Function Programatically
+}); */
