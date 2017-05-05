d7b6f0fb1622c5881a43c8128894b3e9b0994cb8
Robert Weber <rwebaz@me.com>
2017-05-05 13:29:51 -0700

    Move gfm (.md) to Carbon-Free-Footprint Project
    C=> https://github.com/rwebaz/Carbon-Free-Footprint

--- ts/scripts/strict-console.ts
+++ ts/scripts/strict-console.ts
@@ -1,4 +1,6 @@
 //New Typescript Function
+'use strict';
+// Declare an anonymous function
 function log(message: string): void {
   let testLog = "Hello There!";
   if (typeof window.console !=='undefined') {
